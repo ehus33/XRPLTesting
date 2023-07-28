f50aaf0dc69e3022a926dc17c03c2deb50b54704
Ethan Hu <ethanh8093@gmail.com>
2023-07-28 10:20:36 -0700

    Implemented DID Generation

--- public/script.js
+++ public/script.js
@@ -1,21 +1,29 @@
-// Wrap the code inside DOMContentLoaded event listener
+// public/script.js
+import { generateDID } from './did.js';
+
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('checkBtn').addEventListener('click', async () => {
-      const address = document.getElementById('address').value;
-      const resultContainer = document.getElementById('result');
-  
-      try {
-        const response = await fetch(`/balance/${address}`);
-        const data = await response.json();
-  
-        if (response.ok) {
-          resultContainer.innerText = `Balance for ${address}: ${data.balance} XRP`;
-        } else {
-          resultContainer.innerText = `Error: ${data.error}`;
-        }
-      } catch (error) {
-        resultContainer.innerText = `Error: Failed to fetch data`;
+  // Check Balance button click event
+  document.getElementById('checkBtn').addEventListener('click', async () => {
+    const address = document.getElementById('address').value;
+    const resultContainer = document.getElementById('result');
+
+    try {
+      const response = await fetch(`/balance/${address}`);
+      const data = await response.json();
+
+      if (response.ok) {
+        resultContainer.innerText = `Balance for ${address}: ${data.balance} XRP`;
+      } else {
+        resultContainer.innerText = `Error: ${data.error}`;
       }
-    });
+    } catch (error) {
+      resultContainer.innerText = `Error: Failed to fetch data`;
+    }
   });
-  
+
+  // Generate DID button click event
+  document.getElementById('generateBtn').addEventListener('click', () => {
+    const userDID = generateDID();
+    document.getElementById('didOutput').textContent = `Your DID: ${userDID}`;
+  });
+});
